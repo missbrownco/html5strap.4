12be0f47d2f532afa2cac258e895f81579eabb84
Mark Joseph Rivera <imarkdesigns@gmail.com>
2013-09-29 02:20:34 +0800

    init.update - 9.28.13

--- manager/home.php
+++ manager/home.php
@@ -27,16 +27,13 @@
 	<ul class="list-unstyled menu">
 		<li class="device-menu">&nbsp;</li>
 		<li class="home"><a href="#">homepage</a></li>
-		<li><a>html5strap</a>
+		<li><a>sample dropdown menu</a>
 			<ul>
 				<li><a href="#">another</a></li>
 				<li><a href="#">another action</a></li>
 				<li><a href="#">something else here</a></li>
 			</ul>
 		</li>
-		<li><a href="#">bootstrap</a></li>
-		<li><a href="#">ico moon</a></li>
-		<li><a href="#">php inheritance</a></li>
 	</ul>	
 </div>
 <div id="container">
